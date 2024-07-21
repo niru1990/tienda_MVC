b48b142ba8059f9f116c9d5d6f60cefe4eda1e3a
niru1990 <nicolas.reyes90@gmail.com>
2024-07-21 12:19:09 -0300

    diferencias

--- CapaPresentacionAdmin/obj/Debug/net6.0/CapaPresentacionAdmin.AssemblyInfo.cs
+++ CapaPresentacionAdmin/obj/Debug/net6.0/CapaPresentacionAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacionAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c2702fb59dc99ccc50a796b7ca22a3145cce65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d153e6f558214b19031aee19249976e8d92c161")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacionAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacionAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
