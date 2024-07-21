0b5a386e16881093328ded33f1b87930919c83f2
niru1990 <nicolas.reyes90@gmail.com>
2024-07-21 01:10:14 -0300

    se modifica plantilla

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CapaPresentacionAdmin.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
